b695d3c8680f2cccda50a53a85d42ea0c563245f
Eldim5656 <139523051+Eldim5656@users.noreply.github.com>
2023-07-17 19:12:07 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -338,6 +338,11 @@ const blockFileRenameMap = {
 "assets/minecraft/textures/block/peony_top.png": "double_plant_paeonia.png",
 "assets/minecraft/textures/block/rose_bush_bottom.png": "double_plant_rose_bottom.png",
 "assets/minecraft/textures/block/rose_bush_top.png": "double_plant.png",
+"assets/minecraft/textures/block/wooden_sword.png": "wood_sword.png",
+"assets/minecraft/textures/block/wooden_axe.png": "wood_axe.png",
+"assets/minecraft/textures/block/wooden_hoe.png": "wood_hoe.png",
+"assets/minecraft/textures/block/wooden_shovel.png": "wood_shovel.png",
+"assets/minecraft/textures/block/wooden_pickaxe.png": "wood_pickaxe.png",
 };
 
 let selectedFile = null;
